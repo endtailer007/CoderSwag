ed67ca584d6abe7745bde5b6a5412e236b87d8c5
endtailer <surajofficial2000@gmail.com>
2022-08-17 11:05:13 +0530

    commit4

--- app/src/main/java/com/example/coderswag/adapters/CategoryRecycleAdapter.kt
+++ app/src/main/java/com/example/coderswag/adapters/CategoryRecycleAdapter.kt
@@ -11,20 +11,21 @@ import androidx.recyclerview.widget.RecyclerView.Adapter
 import com.example.coderswag.R
 import com.example.coderswag.model.Category
 
-class CategoryRecycleAdapter(val context:Context,val categories:List<Category>): Adapter<CategoryRecycleAdapter.Holder>(){
-    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView){
+class CategoryRecycleAdapter(val context:Context,val categories:List<Category>,val itemClick:(Category)-> Unit): Adapter<CategoryRecycleAdapter.Holder>(){
+    inner class Holder(itemView: View,val itemClick:(Category)->Unit) : RecyclerView.ViewHolder(itemView){
         val categoryImage=itemView.findViewById<ImageView>(R.id.categoryimage)
         val categoryName=itemView.findViewById<TextView>(R.id.categoryName)
         fun bindCategory(category: Category,context: Context){
             val resourceId=context.resources.getIdentifier(category.image,"drawable",context.packageName)
             categoryImage?.setImageResource(resourceId)
             categoryName?.text=category.title
+            itemView.setOnClickListener { itemClick(category) }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {//This method is called when new holders are needed
         val view=LayoutInflater.from(parent.context).inflate(R.layout.category_list_item,parent,false)
-        return Holder(view)
+        return Holder(view,itemClick)
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {//This is used in recycler view display the data at a specified location through process known as binding
--- app/src/main/java/com/example/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/controller/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.coderswag.controller
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
@@ -13,13 +14,18 @@ import com.example.coderswag.adapters.CategoryAdapter
 import com.example.coderswag.adapters.CategoryRecycleAdapter
 import com.example.coderswag.model.Category
 import com.example.coderswag.services.DataService
+import com.example.coderswag.utilities.EXTRA_CATEGORY
 
 class MainActivity : AppCompatActivity() {
     lateinit var adapter:CategoryRecycleAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        adapter = CategoryRecycleAdapter(this,DataService.categories)
+        adapter = CategoryRecycleAdapter(this,DataService.categories){category ->
+            val productIntent= Intent(this,ProductsActivity::class.java)
+            productIntent.putExtra(EXTRA_CATEGORY,category.title)
+            startActivity(productIntent)
+        }
         findViewById<RecyclerView>(R.id.categoryListView).adapter=adapter
         val layoutManager=LinearLayoutManager(this)
         findViewById<RecyclerView>(R.id.categoryListView).layoutManager=layoutManager
